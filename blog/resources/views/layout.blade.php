92b3432fcecfe1257b8f110e9992f44e6837010f
Daniyar Kayirbolatov <daniyarkayirbolatov@gmail.com>
2018-01-08 02:43:40 -0600

    layout compressing or easy for coding

---
+++ blog/resources/views/layout.blade.php
@@ -0,0 +1,26 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Album example for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
+    <!-- Custom styles for this template -->
+    <link href="css/album.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+   @include('layouts.nav')
+   	@yield('content')
+   @include('layouts.footer')
+  </body>
+</html>
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -12,16 +12,4 @@
 */
 use App\Task;
 
-Route::get('/tasks', 'TasksController@index');
-Route::get('/tasks/{task}', 'TasksController@show');
-
-/*Route::get('/tasks', function () {
-	$tasks = DB::table('tasks')->get();
-    return view('tasks.index', compact('tasks'));
-});
-Route::get('/tasks/{task}', function ($id) {
-	//dd($id);
-	//$task = DB::table('tasks')->find($id);
-	$task = Task::find($id);
-    return view('tasks.show', compact('task'));
-});*/
+Route::get('/', 'PostsController@index');
